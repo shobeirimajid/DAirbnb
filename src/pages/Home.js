53ecaa4fb250811e3a9f5f352ca9e72ef6b2ee69
umaresso <umarkhatab467@gmail.com>
2022-12-12 18:59:37 +0500

    initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import bg from "../images/frontpagebg.png"
+import logo from "../images/airbnbRed.png"
 
 const Home = () => {
 
   return (
     <>
-      <Link to="/rentals"> Rentals </Link>
+      <div className="container"  style={{
+        backgroundImage:`url(${bg})`
+      }}>
+
+      </div>
     </>
   );
 };
